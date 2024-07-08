b8b120e82d9430ef2884c7c01a5192ba8679165c
victor <victor.murithi@student.moringaschool.com>
2024-07-08 18:06:20 +0300

    removed nested route

--- frontend/MyJournalApp/navigation/AppNavigator.tsx
+++ frontend/MyJournalApp/navigation/AppNavigator.tsx
@@ -10,12 +10,12 @@ const Stack = createStackNavigator();
 
 const AppNavigator = () => {
   return (
-    <NavigationContainer>
+    <>
       <Stack.Navigator initialRouteName="Login" screenOptions={{ headerShown: false }}>
         <Stack.Screen name="Login" component={LoginScreen} />
         <Stack.Screen name="Signup" component={SignupScreen} />
       </Stack.Navigator>
-    </NavigationContainer>
+    </>
   );
 };
 
