b84fd71f6c771444a3b0cfa0e30743fd26b02b64
victor <victor.murithi@student.moringaschool.com>
2024-07-08 09:30:13 +0300

    removed unwanted files

--- frontend/MyJournalApp/app/index.tsx
+++ frontend/MyJournalApp/app/index.tsx
@@ -1,7 +1,7 @@
 import 'react-native-gesture-handler';
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import AuthNavigator from './navigation/AuthNavigator';
+import AuthNavigator from '../navigation/Authnavigator';
 
 const App = () => {
   return (
